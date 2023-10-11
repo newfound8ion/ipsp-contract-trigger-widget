f18dabf1cff65ce37d09ba8db0f95aa02a36063f
Luke Annison <lukeannison@gmail.com>
2023-10-11 13:16:03 +0100

    activate toggle and screen in

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
+import React, { useState } from "react";
 import Button from "./components/Button";
-import { useState } from "react";
 
 export default function App() {
   const [activating, setActivating] = useState(false);
+
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <div className="flex flex-col items-center justify-center pt-8 pb-8 border border-black w-64 h-96">
-        <p className="text-sm text-center mb-auto">
-          Guild Gitcoin Proof-of-Humanity
-          <br />
-          <br />
-          Condition: Have a Gitcoin Passport with 25 score in Unique Humanity
-          Score.
-          <br />
-          <br />
-          Reward: 100 Verification WATT
-        </p>
+      <div className="relative flex flex-col items-center justify-between pt-8 pb-8 border border-black w-64 h-96">
+        <div className={`${activating ? "opacity-0" : "opacity-100"}`}>
+          <p className="text-sm text-center">
+            Guild Gitcoin Proof-of-Humanity
+            <br />
+            <br />
+            Condition: Have a Gitcoin Passport with 25 score in Unique Humanity
+            Score.
+            <br />
+            <br />
+            Reward: 100 Verification WATT
+          </p>
+        </div>
+        {activating && (
+          <img
+            src="./assets/loading.png"
+            alt="Loading..."
+            className="absolute top-16 w-20 h-20"
+          />
+        )}
         <Button activating={activating} setActivating={setActivating} />
       </div>
     </div>
